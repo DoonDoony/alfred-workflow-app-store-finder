9e731916515f27c06feb9829779ac90429f66fda
DoonDoony <cream.doondoon@gmail.com>
2021-05-24 00:29:14 +0900

    :sparkles: Add some constant

---
+++ src/consts.py
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+
+
+from string import Template
+
+APP_STORE_APPSCHEME = Template("macappstores://apps.apple.com/${country}/app/${app_name}/${app_id}")
+ITUNES_SEARCH_URL = u"https://itunes.apple.com/search"
+IP_INFO_URL = u"https://ipinfo.io/json"
+ICONS_DIR = u"icons"
